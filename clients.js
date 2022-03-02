1f370b63234d1369a6c23df243cb936d54952c96
soltesyzak <yco.soltes@gmail.com>
2022-03-02 13:01:18 +0100

    First commit

---
+++ clients.js
@@ -0,0 +1,17 @@
+const clientsList = [
+    {
+      "name": "Laura",
+      "age": "20",
+      "image": "1.jpeg"
+    },
+    {
+      "name": "Ivana",
+      "age": "19",
+      "image": "2.jpeg"
+    },
+    {
+      "name": "Alexandra",
+      "age": "25",
+      "image":"3.jpeg"
+    }
+]  
