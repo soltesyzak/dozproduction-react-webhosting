8deeddb37cbe97bed039b5a2141d78dc42018fb2
soltesyzak <yco.soltes@gmail.com>
2022-04-13 15:21:02 +0200

    Redesign

--- data/clients.js
+++ data/clients.js
@@ -1,26 +1,120 @@
 const clientsList = [
     {
       "id": 1000,
-      "name": "Laura",
+      "name": "LAURA",
       "age": 20,
-      "images": 2,
       "height": 160,
-      "info": []
+      "sex":"F",
+      "info": [],
+      "photos": [
+        "./media/1000_0.jpeg",
+        "./media/1000_1.jpeg"
+      ],
     },
     {
       "id": 2000,
-      "name": "Ivana",
+      "name": "IVANA",
       "age": 19,
-      "images": 3,
       "height": 160,
-      "info": []
+      "info": [],
+      "sex":"F",
+      "photos": [
+        "./media/2000_0.jpeg",
+        "./media/2000_1.jpeg",
+        "./media/2000_2.jpeg"
+      ]
     },
     {
       "id": 3000,
-      "name": "Alexandra",
+      "name": "ALEXANDRA",
       "age": 25,
-      "images": 3,
       "height": 160,
-      "info": []
+      "info": [],
+      "sex":"F",
+      "photos": [
+        "./media/3000_0.jpeg",
+        "./media/3000_1.jpeg",
+        "./media/3000_2.jpeg"
+      ]
+    },
+    {
+      "id": 3001,
+      "name": "IGOR",
+      "age": 51,
+      "height": 180,
+      "info": [],
+      "sex":"M",
+      "photos": [
+        "./media/IMG-7573.JPG",
+        "./media/IMG-7599.JPG",
+        "./media/IMG-7603.JPG",
+        "./media/IMG-7610.JPG",
+        "./media/IMG-7611.jpg"
+      ]
+    },
+    {
+      "id": 3002,
+      "name": "MARKO",
+      "age": 25,
+      "height": 180,
+      "sex":"M",
+      "info": [],
+      "photos": [
+        "./media/IMG-7721.JPG",
+        "./media/IMG-7719.JPG",
+        "./media/IMG-7720.JPG",
+        "./media/IMG-7722.JPG",
+        "./media/IMG-7723.JPG",
+        "./media/IMG-7724.JPG"
+      ]
+    },
+    {
+      "id": 3003,
+      "name": "VIKTOR",
+      "age": 25,
+      "height": 180,
+      "sex":"M",
+      "info": [],
+      "photos": [
+        "./media/IMG-9225.JPG",
+        "./media/IMG-9224.JPG"
+      ]
+    },
+    {
+      "id": 3004,
+      "name": "ONDREJ",
+      "age": 12,
+      "height": 150,
+      "sex":"M",
+      "info": [],
+      "photos": [
+        "./media/IMG-1240.JPG",
+        "./media/IMG-1239.JPG",
+        "./media/IMG-1238.JPG"
+      ]
+    },
+    {
+      "id": 3005,
+      "name": "DANKO",
+      "age": 12,
+      "height": 150,
+      "info": [],
+      "photos": [
+        "./media/IMG-1712.JPG",
+        "./media/IMG-1713.JPG"
+      ]
+    },
+    {
+      "id": 3006,
+      "name": "VANESA",
+      "age": 12,
+      "height": 150,
+      "sex":"F",
+      "info": [],
+      "photos": [
+        "./media/IMG-1882.JPG",
+        "./media/IMG-1881.JPG",
+        "./media/IMG-1879.JPG"
+      ]
     }
 ]  
